c54594dc5b680eb392c777ea2c0c89b8f621a905
StanislavKhalash <stanislav.khalash@gmail.com>
2016-12-11 10:22:56 +0100

    Refactored menu controller. Added selected menu item marker

--- app/controllers/MenuController.js
+++ app/controllers/MenuController.js
@@ -3,7 +3,40 @@
  */
 
 cookBookApp.controller('MenuController', ['$scope', '$location', function ($scope, $location) {
-    $scope.selectItem = function (path) {
-        $location.path(path);
+    $scope.items = [
+        {
+            routePath: '/dashboard',
+            title: 'Dashboard',
+            icon: 'tachometer'
+        },
+
+        {
+            routePath: '/foods',
+            title: 'Foods',
+            icon: 'shopping-basket'
+        },
+
+        {
+            routePath: '/recipes',
+            title: 'Recipes',
+            icon: 'cutlery'
+        }
+    ];
+
+    $scope.selectedItem = null;
+
+    var updateSelectedItem = function () {
+        var url = $location.url();
+        $scope.selectedItem = $scope.items.find(function (item) {
+            return item.routePath === url;
+        });
     };
+
+    $scope.$on('$locationChangeStart', updateSelectedItem);
+
+    $scope.navigateTo = function (item) {
+        $location.path(item.routePath);
+    };
+
+    updateSelectedItem();
 }]);
