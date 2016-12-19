23a1769fafc4df0dcf9ca1cd4d0ff7faac5ec45c
StanislavKhalash <stanislav.khalash@gmail.com>
2016-12-19 14:27:40 +0100

    Added more screen service tests

--- test/services/screenServiceTest.js
+++ test/services/screenServiceTest.js
@@ -1,14 +1,17 @@
 'use strict';
 
 describe('factory: Screen', function () {
+    var $rootScope;
+
     var createScreen = function (window) {
         module(function ($provide) {
             $provide.value('$window', window);
         });
 
         var screen = {};
-        inject(function (_screen_) {
+        inject(function (_screen_, _$rootScope_) {
             screen = _screen_;
+            $rootScope = _$rootScope_;
         });
 
         return screen;
@@ -16,14 +19,24 @@ describe('factory: Screen', function () {
 
     var createWindow = function (innerWidth) {
         var window = {};
+        var listeners = {};
+
         window.innerWidth = innerWidth;
-        window.addEventListener = jasmine.createSpy('addEventListener');
+        window.addEventListener = function(type, listener) {
+            listeners[type] = listener;
+        };
+        window.triggerEvent = function(type) {
+            var listener = listeners[type];
+            listener(new Event(type), type);
+        };
 
         return window;
     };
 
-    var triggerResize = function (element) {
-        angular.element(element).triggerHandler('resize');
+    var resizeWindow = function (window, newWidth) {
+        window.innerWidth = newWidth;
+        window.triggerEvent('resize');
+        $rootScope.$digest();
     };
 
     beforeEach(function () {
@@ -46,5 +59,19 @@ describe('factory: Screen', function () {
         var window = createWindow(1024);
         var screen = createScreen(window);
         expect(screen.isSmall()).toBe(false);
+    });
+
+    it('should react on window scale up', function() {
+        var window = createWindow(256);
+        var screen = createScreen(window);
+        resizeWindow(window, 1024);
+        expect(screen.isSmall()).toBe(false);
+    });
+
+    it('should react on window scale down', function() {
+        var window = createWindow(1024);
+        var screen = createScreen(window);
+        resizeWindow(window, 256);
+        expect(screen.isSmall()).toBe(true);
     });
 });
